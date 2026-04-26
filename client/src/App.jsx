c4f723613d81e06cabc3e4ec69b6c1aa69669c41
lostboy <alexmuinde470@gmail.com>
2026-04-26 18:43:36 +0300

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/home';
+import SignIn from './pages/signIn';
+import SignUp from './pages/signUp';
+import About from './pages/about';
+import Profile from './pages/Profile';
+
+
+export default function App(){
+  return<BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/sign-in' element={<SignIn/>}/>
+      <Route path='/sign-up' element={<SignUp/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/profile' element={<Profile/>}/>
+    </Routes>
+  </BrowserRouter>
+}
---
+++ client/src/pages/Profile.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Profile(){
+  return(<h1 className="text-red-200  ">Kino Kee</h1>)
+}
