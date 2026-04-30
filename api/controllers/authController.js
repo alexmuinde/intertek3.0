475cd206e32462ec4466c7ea78d69eae825b1209
lostboy <alexmuinde470@gmail.com>
2026-04-30 21:47:23 +0300

    create signIn API route

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,3 +1,54 @@
+const User = require("../models/userModel.js");
+const bcryptjs = require("bcryptjs");
+const { errorHandler } = require("../utils/error.js");
+const { JsonWebTokenError } = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
+
+// Add 'next' here so you can pass errors to your middleware
+exports.signUp = async (req, res, next) => {
+	const { username, email, pin } = req.body;
+
+	try {
+		// 1. Hash the pin
+		const hashedPin = bycrypt.hashSync(pin, 10);
+
+		// 2. Create the new user instance
+		const newUser = new User({ username, email, pin: hashedPin });
+
+		// 3. Save to MongoDB (Must be inside try block)
+		await newUser.save();
+
+		// 4. Send success response
+		res.status(201).json({ message: "User created successfully!" });
+	} catch (error) {
+		// This will now catch duplicate emails or database connection issues
+		next(error);
+	}
+};
+
+exports.signIn = async (req, res, next) => {
+	const { email, pin } = req.body;
+
+	try {
+		//check if the user exist
+		const validUser = await User.findOne({ email });
+		//if the user does not exist
+		if (!validUser) return next(errorHandler(404, "User not found!"));
+		//check if the PIN matches
+		const validPIN = bcryptjs.compareSync(pin, validUser.pin);
+		//if the user does not exist
+		if (!validPIN) return next(errorHandler(401, "Wrong credentials!"));
+		//authenticate the user by creating a token
+		const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+		//before sending the valid user, remove the PIN from the rest
+		const { pin: password, ...rest } = validUser._doc;
+		//save the token as the cookie
+		res.cookie("accessToken", token, { httpOnly: true }).status(200).json(rest);
+	} catch (error) {
+		next(error);
+	}
+};
+
 /**const User = require("../models/userModel.js"); // CORRECT
 
 const bycrypt = require('bcryptjs');
@@ -23,30 +74,3 @@ exports.signUp = async (req, res) => {
     
 
 } */
-
-const User = require("../models/userModel.js");
-const bycrypt = require('bcryptjs');
-const { errorHandler } = require("../utils/error.js");
-
-// Add 'next' here so you can pass errors to your middleware
-exports.signUp = async (req, res, next) => {
-    const { username, email, pin } = req.body;
-
-    try {
-        // 1. Hash the pin
-        const hashedPin = bycrypt.hashSync(pin, 10);
-
-        // 2. Create the new user instance
-        const newUser = new User({ username, email, pin: hashedPin });
-
-        // 3. Save to MongoDB (Must be inside try block)
-        await newUser.save();
-
-        // 4. Send success response
-        res.status(201).json({ message: "User created successfully!" });
-
-    } catch (error) {
-        // This will now catch duplicate emails or database connection issues
-        next(error); 
-    }
-};
