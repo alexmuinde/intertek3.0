457864fa1f0a8006115619659ee9126f53bb2c3b
lostboy <alexmuinde470@gmail.com>
2026-05-01 01:16:36 +0300

    Add redux persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,21 +1,57 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userReducer from "./user/userSlice.js";
-/**const configureStore = require("@reduxjs/toolkit");
-const { useReducer } = require("./user/userSlice.js"); */
+import { persistReducer, persistStore } from "redux-persist";
+
+// Custom storage object to bypass the broken import
+const storage = {
+	getItem: (key) => Promise.resolve(localStorage.getItem(key)),
+	setItem: (key, value) => Promise.resolve(localStorage.setItem(key, value)),
+	removeItem: (key) => Promise.resolve(localStorage.removeItem(key)),
+};
+
+const rootReducer = combineReducers({ user: userReducer });
+
+const persistConfig = {
+	key: "root",
+	storage, // Uses our custom storage defined above
+	version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-	reducer: { user: userReducer },
+	reducer: persistedReducer,
 	middleware: (getDefaultMiddleware) =>
 		getDefaultMiddleware({
 			serializableCheck: false,
 		}),
 });
 
-/**const store = configureStore({
-	reducer: { user: useReducer },
+export const persistor = persistStore(store);
+
+/**import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import userReducer from "./user/userSlice.js";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import persistStore from "redux-persist/es/persistStore";
+
+const rootReducer = combineReducers({ user: userReducer });
+
+const persistConfig = {
+	key: "root",
+	storage,
+	version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+	reducer: persistedReducer,
 	middleware: (getDefaultMiddleware) =>
 		getDefaultMiddleware({
 			serializableCheck: false,
 		}),
 });
-modules.exports = store; */
+
+export const persistor = persistStore(store);
+ */
